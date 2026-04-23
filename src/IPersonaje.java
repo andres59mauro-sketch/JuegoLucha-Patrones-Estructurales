dfd94df49afd10bcc55c3cb0d0b10bfefa473b4e
andres59mauro-sketch <andres59mauro@gmail.com>
2026-04-22 21:07:00 -0500

    Delete src/IPersonaje.java

--- src/IPersonaje.java
+++
@@ -1,7 +0,0 @@
-public interface IPersonaje {
-    void atacar(IPersonaje oponente);
-    void recibirDano(int dano);
-    boolean estaVivo();
-    String getNombre();
-    int getPuntosDeVida();
-}
